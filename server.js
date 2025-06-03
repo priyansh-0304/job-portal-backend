d4eb1765232851e12d48948f43af5b0ebc6ef311
Priyansh <arorapriyansh0304@gmail.com>
2025-06-03 23:59:15 +0530

    Initial commit

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://job-portal-frontend-rb5jzidyq-priyansh-0304s-projects.vercel.app',
+  origin: true,
   credentials: true,
 }));
 
 app.use((req, res, next) => {
   //res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Update with your frontend URL
-  res.header('Access-Control-Allow-Origin', 'https://job-portal-frontend-rb5jzidyq-priyansh-0304s-projects.vercel.app');
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
